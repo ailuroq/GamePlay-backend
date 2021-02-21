09fc997c888a6a29353729f0558133d72b97e02a
champion2030 <yar-resh@yandex.ru>
2021-02-21 23:15:54 +0300

    testing github xd

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,4 @@ async function bootstrap() {
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+//Hello, it's my git branch!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! LOOOOOOOOOOOOOOOOOOOOL NICE DAY!
