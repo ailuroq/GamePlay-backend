ec0b31eee496660e94f2071e0785291f5b48f14c
ailuroq <yaroslav_ovsyannikov@list.ru>
2021-05-21 02:39:20 +0300

    file uploading fix

--- src/user/controllers/file.uploading.controller.ts
+++ src/user/controllers/file.uploading.controller.ts
@@ -22,30 +22,39 @@ export class FileUploadingController {
   @UseGuards(JwtAuthGuard)
   @Post('upload/avatar')
   @UseInterceptors(
-    FileInterceptor('image', { /**/
-      storage: diskStorage({  /**/
-        destination: './uploads', /**/
-        filename: (req, file, callback) => {  /**/
-          const name = file.originalname.split('.')[0]; /**/
-          const fileExtName = extname(file.originalname); /**/
-          const randomName = Array(8) /**/
-            .fill(null) /**/
-            .map(() => Math.round(Math.random() * 16).toString(16)) /**/
-            .join('');  /**/
-          const finalFileName = `${name}-${randomName}${fileExtName}`;  /**/
-          callback(null, finalFileName);  /**/
-        },  /**/
-      }), /**/
-      fileFilter: (req, file, callback) => {  /**/
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {  /**/
-          return callback(new Error('Only image files are allowed!'), false); /**/
-        } /**/
-        callback(null, true); /**/
-      },  /**/
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const name = file.originalname.split('.')[0];
+          const fileExtName = extname(file.originalname);
+          const randomName = Array(8)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
+          const finalFileName = `${name}-${randomName}${fileExtName}`;
+          callback(null, finalFileName);
+        },
+      }),
+      fileFilter: (req, file, callback) => {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+          return callback(new Error('Only image files are allowed!'), false);
+        }
+        callback(null, true);
+      },
     }),
   )
-  async uploadFile(@UploadedFile() file, @Request() req) {  /**/
-    return this.userService.createAvatar(file.filename, req.user);  /**/
-  } /**/
+  async uploadFile(@UploadedFile() file, @Request() req) {
+    return this.userService.createAvatar(file.filename, req.user);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Post('delete/avatar')
+  async deleteAvatar(@Request() req) {
+    return this.userService.deleteAvatar(req.user);
+  }
 
+  @Get('uploads/:imgPath')
+  seeUploadedFile(@Param('imgPath') image, @Res() res) {
+    return res.sendFile(image, { root: './uploads' });
+  }
 }
